3de8fd6e19bef7b2c50e59232b4c9d91696602c5
zpx2012 <zhupengxiong@yeah.net>
2020-08-21 17:57:50 -0700

    [err]

--- src/http.cc
+++ src/http.cc
@@ -2264,9 +2264,10 @@ HttpStateData::sendRequest()
     //serverConnection.local.port()
     //serverConnection.remote.toStr(buf, len)
     //serverConnection.remote.port()
-    char local_ip[16], remote_ip[16];
+    char remote_ip[16];//local_ip[16], 
     // std::cout << serverConnection->local.toStr(local_ip, 16) << serverConnection->local.port() << serverConnection->remote.toStr(remote_ip, 16) << serverConnection->remote.port() << std::endl;
-    system("iptables -A INPUT -p tcp -s " << remote_ip << "--sport " << serverConnection.remote.port() << "--dport " << serverConnection.local.port() << "-j NFQUEUE --queue-num 6");
+    serverConnection->remote.toStr(remote_ip, 16);
+    system(std::string("iptables -A INPUT -p tcp -s ") + remote_ip + "--sport " + serverConnection->remote.port() + "--dport " + serverConnection->local.port() + "-j NFQUEUE --queue-num 6");
     debugs(11, 2, "HTTP Server " << serverConnection);
     debugs(11, 2, "HTTP Server REQUEST:\n---------\n" << mb.buf << "\n----------");
 
