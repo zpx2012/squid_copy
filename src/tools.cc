675abd98aea1cc43144a01a217f515262311e6d4
zpx2012 <zhupengxiong@yeah.net>
2020-08-27 17:35:31 -0700

    [bug] comment out SIGSEG handling

--- src/tools.cc
+++ src/tools.cc
@@ -290,9 +290,10 @@ PrintRusage(void)
 void
 death(int sig)
 {
-    if (sig == SIGSEGV)
-        debugs(1, DBG_CRITICAL, ForceAlert << "FATAL: Received Segment Violation...dying.");
-    else if (sig == SIGBUS)
+    // if (sig == SIGSEGV)
+    //     debugs(1, DBG_CRITICAL, ForceAlert << "FATAL: Received Segment Violation...dying.");
+    // else 
+    if (sig == SIGBUS)
         debugs(1, DBG_CRITICAL, ForceAlert << "FATAL: Received Bus Error...dying.");
     else
         debugs(1, DBG_CRITICAL, ForceAlert << "FATAL: Received signal " << sig << "...dying.");
